f8a86733b28cd01f4499c25dec20d8da52a11404
sct213 <yang991211@naver.com>
2020-08-10 01:43:29 +0000

    8/10

---
+++ interface.java
@@ -0,0 +1,14 @@
+public interface1 {
+  public int calculator(int v1, int v2, char c) {}
+  public int plus(int num, int num2) {}
+}
+
+public class interface {
+  public static void main(String[] args) {
+    @Override
+    public int calculator(int v1, int v2, char c) {
+      Scanner sc = new Scanner(System.in);
+      
+    }
+  }
+}

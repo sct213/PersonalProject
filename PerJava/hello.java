4de895974a4f3421508ac171bfeeb015e427bebe
sct213 <yang991211@naver.com>
2020-08-18 10:04:17 +0900

    개강

--- PerJava/hello.java
+++ PerJava/hello.java
@@ -1,7 +1,23 @@
+interface calculator {
+  public int plus();
+  public int minus();
+}
 public class hello {
-  public static void main(String[] args) {
-    System.out.println("hello");
-    System.out.println("hello2");
+    @Override
+    public int plus(int v1, int v2){
+      System.out.println("결과: " + v1 + v2);
+    }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int num = sc.nextInt();
+      int num1 = sc.nextInt();
+      plus(num, num1);
+    }
   }
 }
-// This Text is Good
+
+
+
+
+
+

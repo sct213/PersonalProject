de4d4948b90feb99a4bbbee60ce6acee8a027390
sct213 <yang991211@naver.com>
2020-09-10 02:08:27 +0000

    test - linux

--- PerJava/Untitled.java
+++ PerJava/Untitled.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 class Untitled {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		String anwser = sc.nextLine();
+
 		System.out.println("Hello, world");
 	}
 }
